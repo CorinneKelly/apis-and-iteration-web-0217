bff16cc389b4b59d4d04690d663f3f13a855a424
CorinneKelly <corinnemariekelly@gmail.com>
2017-02-08 16:29:44 -0500

    edits based on star wars lecture - unfinished

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,36 +2,60 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
+class Character
+  attr_reader :name, :film_urls
+  def initialize(character_hash)
+    @name = character_hash["name"]
+    @film_urls = character_hash["films"]
+  end
+end
+
+character = Character.new("Han Solo")
+character.name # => "Han Solo"
+
+def get_all_characters
+  puts "Be patient you must.. getting characters"
+
+  url = 'http://www.swapi.co/api/people/'
+  all_characters = []
+  begin
+    characters_raw =RestClient.get(url).body
+    result = JSON.parse(characters_raw)
+    all_characters += result["results"]
+    url = result["next"]
+  end while url
+
+  all_characters.map do |character|
+    Character.new(character)
+  end
+
+end
+
+def find_character(characters, character_name)
+  characters.find do |character|
+    name = character.name
+    name.downcase == character_name.downcase
+  end
+end
+
+def find_films_for(character)
+  film_urls = character.film_urls #need to know where character is
+  film_urls.map do |film|
+    result = RestClient.get(film)
+    JSON.parse(result)
+  end
+end
+
+def get_character_movies_from_api(character_name)
   #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
-  
-  films = []
-  character_hash["results"].each do |char|
-    if char["name"] == character
-      films = char["films"]
-    end
-  end
+  all_characters = get_all_characters
+  found_character = find_character(all_characters, character_name)
+  films = find_films_for(found_character)
+  puts "#{found_character.name} has starred in:"
 
-  get_hashes_from_urls(films)
-  film_array = []
-  films.each do |film|
-    film_client = RestClient.get(film)
-    film_array << JSON.parse(film_client)
+  films.each.with_index(1) do |film, index|
+    puts "#{index}. #{film["title"]}"
   end
-
-  film_array
 
 end
 
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,37 +1,40 @@
-require_relative "api_communicator.rb"
-
+require_relative "../lib/api_communicator.rb"
+require_relative "../lib/command_line_interface.rb"
 
 def welcome
   # puts out a welcome message here!
+  puts "*" * 34
   puts "Welcome to the Star Wars database!"
-
+  puts "*" * 34
 end
 
 def check_valid_answer(string)
 	until string == "yes" || string == "no"
 		puts "Please say 'yes' or 'no'"
-		string = gets.strip
+		string = gets.strip.downcase
 	end
 	string
 end
 
 def either_or
 	puts "Would you like to look up what movies your favorite Star Wars character was in?"
-	character_answer = gets.strip
+	character_answer = gets.strip.downcase
 	character_answer = check_valid_answer(character_answer)
 
 	if character_answer == "yes"
 		character = get_character_from_user
-		show_character_movies(character)
+		get_character_movies_from_api(character)
+		# show_character_movies(character)
 	end
 
 	puts "Would you like to look up other information about a specific movie?"
-	movie_answer = gets.strip
+	movie_answer = gets.strip.downcase
 	movie_answer = check_valid_answer(movie_answer)
 
 	if movie_answer == "yes"
 		movie_name = get_movie_name_from_user
 		category_name = get_category_name_from_user
+		puts "*" * 34
 		show_movie_info(movie_name, category_name)
 	end
 
@@ -41,17 +44,17 @@ end
 
 def get_character_from_user
   puts "please enter a character"
-  character_name = gets.strip
+  character_name = gets.strip.downcase
   character_name
   # use gets to capture the user's input. This method should return that input, downcased.
 end
 
 def get_movie_name_from_user
 	puts "Please enter a Movie Name"
-	movie_name = gets.strip
+	movie_name = gets.strip.downcase
 end
 
 def get_category_name_from_user
 	puts "Please enter a Category"
-	category_name = gets.strip
+	category_name = gets.strip.downcase
 end
